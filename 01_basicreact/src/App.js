8744f425cb2ff7c24ede73a7c44824d3bcfa434e
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-08-18 18:14:48 +0530

    created two react projects

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Happy To Learn React</h1>
+  );
+}
+
+export default App;
---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+  return (
+    <h1>Happy To Learn React with Vite || Priyanka </h1>
+
+    )
+}
+
+export default App
