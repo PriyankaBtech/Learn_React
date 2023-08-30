15f20dbb301d5b22ef14c713a3f7c2de483d6505
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-08-30 16:19:30 +0530

    props with tailwindcss in react

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let details = {
+    username: 'priyanka',
+    age: 23,
+    place: 'delhi'
+  }
+
+  let myArr = [1, 2, 3, 4]
+
   return (
     <>
-      <h1 className="bg-green-400 text-black text-3xl font-bold p-5 rounded-xl">
+      <h1 className="bg-green-400 text-black text-3xl font-bold p-5 rounded-xl mb-4">
       Test Tailwind
       </h1>
+      <Card username="one" btnText="Click me"/>
+      <Card username="two" btnText="Visit me"/>
     </>
   )
 }
