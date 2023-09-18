40795c9a24d1a3bac1cec975941e690c1aac389e
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-09-18 23:28:26 +0530

    learn react-router by building a webapp

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -8,6 +8,8 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import { Route } from 'react-router-dom'
+import User from './components/User/User.jsx'
+import Github, { GithubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -38,6 +40,12 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route
+      loader={ GithubInfoLoader } 
+      path='github' 
+      element={<Github/>}
+      />
     </Route>
   )
 )
