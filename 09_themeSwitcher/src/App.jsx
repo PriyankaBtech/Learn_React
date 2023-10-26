3aec6c799750e1d575411a0e77c3b89446524fba
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-26 23:41:35 +0530

    setup project

---
+++ 09_themeSwitcher/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>    
+      <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  {/* {theme button} */}
+                    
+                </div>
+          
+                <div className="w-full max-w-sm mx-auto">
+                  {/* {card button} */}
+                  
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
