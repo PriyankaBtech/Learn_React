3ea91f3735f617b319a79553601222a583593f55
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-11-04 23:18:01 +0530

    add side-effect

--- 10_todo/src/App.jsx
+++ 10_todo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
 import './App.css'
 
@@ -26,8 +26,23 @@ function App() {
     setTodos((content) => content.map((prevContent) => prevContent === id ? {...prevContent, toggleCheck: !prevContent.toggleCheck} : prevContent ))
   }
 
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+
+
   return (
-    <TodoProvider value={{addTodo, updateTodo, deleteTodo, toggleCheck}}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCheck}}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
